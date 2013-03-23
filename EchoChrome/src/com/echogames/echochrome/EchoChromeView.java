876c72b5887784d1bbc2aec45b74c4b6b7c1c6c8
psybobah <devnull@localhost>
2013-03-23 22:35:23 +0400

    Initial version of android project

---
+++ EchoChrome/src/com/echogames/echochrome/EchoChromeView.java
@@ -0,0 +1,149 @@
+package com.echogames.echochrome;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Scroller;
+
+public class EchoChromeView extends View {
+    private static final String TAG = "EchoChromeView";
+    
+    private static final float AXIS_X_MIN = -1f;
+    private static final float AXIS_X_MAX = 1f;
+    private static final float AXIS_Y_MIN = -1f;
+    private static final float AXIS_Y_MAX = 1f;
+    
+	private RectF mCurrentViewport = new RectF(AXIS_X_MIN, AXIS_Y_MIN, AXIS_X_MAX/4, AXIS_Y_MAX/4);
+    private Rect mContentRect = new Rect();
+    private float mScale;
+	private Scroller mScroller;
+    private Paint mDataPaint;
+    private float mDataThickness = 2.0f;
+    private int mDataColor = 0xff00aa00;
+    private GestureDetectorCompat mGestureDetector;
+    
+    public EchoChromeView(Context context) {
+        this(context, null, 0);
+    }
+    
+    public EchoChromeView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    
+    public EchoChromeView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        
+        mScroller = new Scroller(context);
+       
+        mDataPaint = new Paint();
+        mDataPaint.setStrokeWidth(mDataThickness);
+        mDataPaint.setColor(mDataColor);
+        mDataPaint.setStyle(Paint.Style.STROKE);
+        mDataPaint.setAntiAlias(true);
+        
+        mGestureDetector = new GestureDetectorCompat(context, mGestureListener);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mContentRect.set(
+                getPaddingLeft(),
+                getPaddingTop(),
+                getWidth() - getPaddingRight(),
+                getHeight() - getPaddingBottom());
+        // make same proportions as for content rectangle to maintain circle shape
+        mCurrentViewport.top  = AXIS_Y_MIN;
+        mCurrentViewport.bottom = AXIS_Y_MIN + mCurrentViewport.width()*mContentRect.height()/mContentRect.width();
+        mCurrentViewport.right = AXIS_X_MIN + mCurrentViewport.width();
+        mCurrentViewport.left = AXIS_X_MIN;
+        mScale = mContentRect.height()/mCurrentViewport.height();
+    }
+    
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    	//Log.d(TAG, "onDraw at " + mCurrentViewport.left + ", " + mCurrentViewport.top);
+        // Clips the next few drawing operations to the content area
+        int clipRestoreCount = canvas.save();
+        canvas.clipRect(mContentRect);
+
+    	canvas.drawCircle(-mCurrentViewport.left*mScale, -mCurrentViewport.top*mScale,
+    			          0.9f*AXIS_Y_MAX*mScale, mDataPaint);
+
+        // Removes clipping rectangle
+        canvas.restoreToCount(clipRestoreCount);
+    }        
+
+    private void setViewportBottomLeft(float x, float y) {
+        /**
+         * Constrains within the scroll range. The scroll range is simply the viewport extremes
+         * (AXIS_X_MAX, etc.) minus the viewport size. For example, if the extrema were 0 and 10,
+         * and the viewport size was 2, the scroll range would be 0 to 8.
+         */
+        float curWidth = mCurrentViewport.width();
+        float curHeight = mCurrentViewport.height();
+        x = Math.max(AXIS_X_MIN, Math.min(x, AXIS_X_MAX - curWidth));
+        y = Math.max(AXIS_Y_MIN + curHeight, Math.min(y, AXIS_Y_MAX));
+
+        mCurrentViewport.set(x, y - curHeight, x + curWidth, y);
+        ViewCompat.postInvalidateOnAnimation(this);
+    }        
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean retVal = mGestureDetector.onTouchEvent(event);
+        return retVal || super.onTouchEvent(event);
+    }
+    
+    /**
+     * The gesture listener, used for handling simple gestures such as double touches, scrolls,
+     * and flings.
+     */
+    private final GestureDetector.SimpleOnGestureListener mGestureListener
+            = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            mScroller.forceFinished(true);
+            ViewCompat.postInvalidateOnAnimation(EchoChromeView.this);
+            return true;
+        }
+        
+        @Override     
+        public boolean onSingleTapUp(MotionEvent e) {
+        	Log.d(TAG, "onSingleTapUp: " + e.toString());
+        	ViewCompat.postInvalidateOnAnimation(EchoChromeView.this);
+        	return true;
+        } 
+        
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            // Scrolling uses math based on the viewport (as opposed to math using pixels).
+            /**
+             * Pixel offset is the offset in screen pixels, while viewport offset is the
+             * offset within the current viewport. For additional information on surface sizes
+             * and pixel offsets, see the docs for {@link computeScrollSurfaceSize()}. For
+             * additional information about the viewport, see the comments for
+             * {@link mCurrentViewport}.
+             */
+            float viewportOffsetX = distanceX * mCurrentViewport.width() / mContentRect.width();
+            float viewportOffsetY = distanceY * mCurrentViewport.height() / mContentRect.height();
+            setViewportBottomLeft( mCurrentViewport.left + viewportOffsetX,
+                                   mCurrentViewport.bottom + viewportOffsetY);
+
+            return true;
+        }
+    };
+
+}
